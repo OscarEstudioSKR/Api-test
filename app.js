7c1ce7d253ac321140cf1c37e196c2f82faea80b
Oscar <oscaraparisisancho@gmail.com>
2019-04-15 18:45:54 +0200

    Error use

--- app.js
+++ app.js
@@ -24,4 +24,19 @@ app.use(cookieParser());
 
 app.use('/books', booksRouter);
 
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({code: 'not found'});
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({code: 'unexpected'});
+  }
+});
+
 module.exports = app;
