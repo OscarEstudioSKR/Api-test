8476baf5b1780795763e560d99d4b27ccdc97a95
Oscar <oscaraparisisancho@gmail.com>
2019-04-15 15:51:35 +0200

    Conect to DB

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const booksRouter = require('./routes/books');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/api-test', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+});
 
 app.use(logger('dev'));
 app.use(express.json());
