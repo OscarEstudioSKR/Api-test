4f45b7dc650fc6ccca20802a179d149fd19d7dda
Oscar <oscaraparisisancho@gmail.com>
2019-04-15 21:28:02 +0200

    Configure sesion

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const booksRouter = require('./routes/books');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -15,6 +18,19 @@ mongoose.connect('mongodb://localhost/api-test', {
   reconnectTries: Number.MAX_VALUE,
 });
 
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'fake-secret',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +39,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/books', booksRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
